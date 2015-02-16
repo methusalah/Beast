729e2dba48e4a2eb81fd3a81716e76f303f9416d
methusalah <ben.oeilnoir@gmail.com>
2015-02-16 17:47:15 +0100

    synchro

--- BeastEclipseProject/src/model/universe/Ressource.java
+++ BeastEclipseProject/src/model/universe/Ressource.java
@@ -2,6 +2,12 @@ package model.universe;
 
 public class Ressource {
 
+	private static double Q_MAX_MAX = 100;
+	private static double Q_GROW_MAX = 100;
+	private static double Q_HARVEST_MAX = 100;
+	private static double CAN_DISAPEAR_PROB = 0.5;
+	private static double SPONTANEOUS_PROB = 0.5;
+	
 	int id;
 	double qMax;
 	double qGrow;
